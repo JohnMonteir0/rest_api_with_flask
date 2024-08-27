52d004a9d8a94250bc42c5728afdc593be920854
johnmonteiro <john.monteiro.brlink@grancursosonline.com.br>
2024-08-27 16:10:44 -0300

    Refactor: adjustment

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 from application import create_app
 import os
 
-if os.getenv('FLASK_ENV') == "development":
+if os.getenv('FLASK_DEBUG') == "development":
   app = create_app('config.DevConfig')
 else:
   app = create_app('config.ProdConfig')
